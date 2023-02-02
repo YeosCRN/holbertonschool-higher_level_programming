96451f530b7fec4155bd70d5d2fef0289b14d00e
YeosCRN <jeoscrn20@outlook.com>
2023-02-02 12:30:39 -0500

    4.0

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if idx < 0 or idx > len(new_list):
+        return new_list
+    for i in range(len(new_list)):
+        if i == idx:
+            new_list[i] = element
+            return new_list
+    return new_list
