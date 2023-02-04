94b398dc4b2d5fc90c169f69055b3d6a9bce2d5d
YeosCRN <jeoscrn20@outlook.com>
2023-02-03 20:48:08 -0500

    0 to 6 no main

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -14,15 +14,3 @@ def safe_print_division(a, b):
         print('Inside result: {}'.format(quotient))
 
     return quotient
-
-
-if __name__ == '__main__':
-    a = 12
-    b = 2
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
-
-    a = 12
-    b = 0
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
