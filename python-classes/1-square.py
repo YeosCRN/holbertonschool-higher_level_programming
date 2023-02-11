2d08e2f104f349b0d97e47a65eeba6ffd08f758b
YeosCRN <jeoscrn20@outlook.com>
2023-02-10 19:07:44 -0500

    test-to-repo

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Square class."""
+
+    def __init__(self, size):
+        """__init__ method for Square.
+
+        Args:
+            size (int): size of Square.
+
+        """
+        self.__size = size
