bff1783bc15cea2366262e233419924f12f0e839
YeosCRN <jeoscrn20@outlook.com>
2023-04-17 16:14:13 -0500

    semi

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let reserve = []
-  let y = 0;
+  const reserve = [];
   for (let x = list.length - 1; x >= 0; x--) {
     reserve.push(list[x]);
   }
