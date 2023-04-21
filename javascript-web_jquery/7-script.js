602f0db7f6928ad00d63263923113cc779131ada
YeosCRN <jeoscrn20@outlook.com>
2023-04-21 17:05:06 -0500

    test_to_rep_recheck2

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+  success: function (data) {
+    $('#character').text(data['name']);
+  },
+  dataType: 'json'
+});
